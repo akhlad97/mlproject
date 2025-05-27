4be972a040d7b48905e9309ad8293ae55940b53c
akhlad97 <akhlad.contact@gmail.com>
2025-05-27 21:05:21 +0530

    create the best model

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,11 @@ import sys
 import numpy as np 
 import pandas as pd 
 import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+from src.exception import CustomException
 from src.exception import CustomException
 
 def save_object(file_path,obj):
@@ -17,4 +22,36 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
         
-    
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model_name = list(models.keys())[i]
+            para = param[model_name]
+
+            print(f"Evaluating model: {model_name}")
+            print(f"Model object: {model}")
+            print(f"Params: {para}")
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
